b7efb4a4a9873036927527a7973ebfb2ea50fd42
Anna MacDonald <anna.m.macdonald@gmail.com>
2021-06-07 18:33:51 -0700

    Update logic, update apollo client

--- apollo-client.js
+++ apollo-client.js
@@ -1,17 +1,21 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
+
 const client = new ApolloClient({
   uri: "https://arweave.net/graphql",
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          transactions: relayStylePagination(),
-        },
-      },
-    },
-  }),
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
 });
 
 export default client;
--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,12 @@ import { runRelayer, processData } from "../lib/posts";
 
 import styles from "../styles/index.module.scss";
 
-const first = 10;
-// after is cursor from prev page
-const delay = true;
-
-// query on load, process data then
-// on click of button, query next group and process data
-
 const MIRROR_QUERY = gql`
-  query Mirror($first: Int, $after: String) {
+  query Mirror($after: String) {
     transactions(
-      first: $first
+      first: 5
       after: $after
+      sort: HEIGHT_ASC
       owners: ["Ky1c1Kkt-jZ9sY1hvLF5nCf6WWdBhIU5Un_BMYh-t3c"]
       tags: [{ name: "App-Name", values: ["MirrorXYZ"] }]
     ) {
@@ -34,19 +28,6 @@ const MIRROR_QUERY = gql`
     }
   }
 `;
-//
-// async function getDataById(posts) {
-//   const postList = [];
-//
-//   for (const transaction of posts) {
-//     const postTX = transaction.node.id;
-//     const item = await generateMirrorURL(postTX);
-//     posts.push(item);
-//   }
-//
-//   return posts;
-// }
-//
 
 function tryParseJSON(jsonString) {
   try {
@@ -73,78 +54,31 @@ export async function getStaticProps() {
 
 export default function Home({ posts }) {
   const [postlist, setPostlist] = useState(posts);
+  const parsed = tryParseJSON(postlist) ? JSON.parse(postlist) : postlist;
+  const [parsedList, setParsedList] = useState(parsed);
+  const [loading, setLoading] = useState(false);
 
-  async function handleClick(cursor) {
-    console.log(`CLICK CURSOR = ${cursor}`);
-    const { data } = await client.query({
-      query: MIRROR_QUERY,
-      variables: {
-        first: 5,
-        after: cursor,
-      },
-    });
-    const list = await processData(data);
-    debugger;
-    const originalList = tryParseJSON(postlist)
-      ? JSON.parse(postlist)
-      : postlist;
-    const combined = [...originalList, ...list];
-    setPostlist(combined);
-    return combined;
+  async function handleClick() {
+    const cursor = parsedList[parsedList.length - 1].cursor;
+    if (cursor) {
+      setLoading(true);
+      const { data } = await client.query({
+        query: MIRROR_QUERY,
+        variables: {
+          after: cursor,
+        },
+      });
+      // fetch new posts
+      const list = await processData(data);
+      setLoading(false);
+      // add new posts to existing list
+      const combined = [...parsedList, ...list];
+      // updated parsedList
+      setParsedList(combined);
+      return combined;
+    }
   }
 
-  // useEffect(() => setPosts(props.data));
-  // const [
-  //   mirrorQuery,
-  //   { error, data, loading, fetchMore, networkSTATUS },
-  // ] = useLazyQuery(MIRROR_QUERY, {
-  //   variables: { first, delay },
-  //   notifyOnNetworkStatusChange: true,
-  // });
-  // const nodes = data && data.transactions.edges.map((edge) => edge.node);
-  //
-  // const postList = getDataById(nodes);
-  //
-  // const observerRef = useRef(null);
-  // const [buttonRef, setButtonRef] = useState(null);
-  //
-  // useEffect(() => {
-  //   const options = {
-  //     root: document.querySelector("#list"),
-  //     threshold: 0.1,
-  //   };
-  //   observerRef.current = new IntersectionObserver((entries) => {
-  //     const entry = entries[0];
-  //     if (entry.isIntersecting) {
-  //       entry.target.click();
-  //     }
-  //   }, options);
-  // }, []);
-  //
-  // useEffect(() => {
-  //   if (buttonRef) {
-  //     observerRef.current.observe(document.querySelector("#buttonLoadMore"));
-  //   }
-  // }, [buttonRef]);
-  //
-  // if (error) {
-  //   console.log(error);
-  //   return <div>Error</div>;
-  // }
-  //
-  // //
-  // // if (networkStatus === 1) {
-  // //   return <div>Loading...</div>;
-  // // }
-  // //
-  // // const isRefetching = networkStatus === 3;
-  //
-  console.log(`POSTS: ${postlist}`);
-  const parsedList = tryParseJSON(postlist) ? JSON.parse(postlist) : postlist;
-  debugger;
-  const cursor = parsedList[parsedList.length - 1].cursor;
-  console.log(`CURSOR = ${cursor}`);
-  // console.log(postList);
   return (
     <div>
       <div className="container">
@@ -154,6 +88,7 @@ export default function Home({ posts }) {
         </Head>
 
         <main>
+          <div>{loading && `Loading...`}</div>
           <div id="list">
             <ul>
               {parsedList.map((post) => (
@@ -162,14 +97,15 @@ export default function Home({ posts }) {
             </ul>
           </div>
           <div className={styles.row}>
-            <div
+            <button
               className={styles.btn}
+              disabled={loading}
               size="mini"
               id="buttonLoadMore"
-              onClick={() => handleClick(cursor)}
+              onClick={() => handleClick()}
             >
-              load more
-            </div>
+              Load more
+            </button>
           </div>
         </main>
 
